923c07610c68616082a0ca43426500766a4bf247
sbolwar <sushil.bolwar@gmail.com>
2017-01-10 23:03:02 +0530

    Just testing jenkins job by adding changes into repository

---
+++ AnotherCheckIn.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class AnotherCheckIn {
+
+	public static void main(String[] args) {
+			for (int i=1; i<=10; i++){
+			System.out.println("AnotherCheckin.........."+i);
+		}
+
+	}
+
+}
